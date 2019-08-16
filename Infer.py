01d4d748004fbe5b9909070f11f182db38d5ecfb
nohseonghun <jzi040941@naver.com>
2019-08-16 15:55:46 +0900

    Infer.py newer version

--- Infer.py
+++ Infer.py
@@ -1,7 +1,8 @@
 from model.fcn_tensorflow import fcn_timetableseg,fcn_daytimetbseg
-from utils import Input_Images,CropImage
+from utils import Input_Images,CropImage,finddaytime
 import cv2
 from model.keras_retinanet import detectScheduled
+from model.Tesseract_With_attention import DeepPos_V2_xywh
 InputImageDir_Path = "zoo/data_zoo/Input_Images"
 def main():
     rawimage_path = Input_Images.ConvertToFcninput(InputImageDir_Path)
@@ -10,4 +11,9 @@ def main():
     ScheduleList = detectScheduled.Inference(croped_timetable)
     fcn_daytimetbseg_outputlabel = fcn_daytimetbseg.Inference(croped_timetable)
     table,day,time = CropImage.CropByDaytimetbFCNLabel(fcn_daytimetbseg_outputlabel, croped_timetable)
+    words, pos = DeepPos_V2_xywh.process(time.Img)
+    time.setratioboxesList(pos)
+    timetable = finddaytime.TimeTable(ScheduleList, words, time)
+    scheduleInfos = timetable.getScheduleInfos()
+    print(scheduleInfos)
 main()
